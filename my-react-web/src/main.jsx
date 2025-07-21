f552526c0bcee3375091f759e4c956a7fe8f6f00
zhe33333 <a0905183052@gmail.com>
2025-07-21 20:39:16 +0800

    Add new pages and assets, remove old .git_disabled directory

--- my-react-web/src/main.jsx
+++ my-react-web/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HashRouter } from 'react-router-dom'; // 1. 從這裡改變
 import "./styles/index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <HashRouter> {/* 2. 從這裡改變 */}
+      <App />
+    </HashRouter>
   </StrictMode>
 );
