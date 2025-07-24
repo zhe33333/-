70b0193da49718da7b3af4b08d2e9144bd64c0c9
zhe33333 <a0905183052@gmail.com>
2025-07-24 21:26:16 +0800

    Update vite.config.js and add package-lock.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/-/', // <--- 確保這一行絕對存在！
 })
